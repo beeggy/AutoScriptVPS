91e78e0e4c96dc88cb7d5f5b40e97516e61a3e8a
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:15:26 +0800

    Update clearcache.sh

--- Files/Menu/clearcache.sh
+++ Files/Menu/clearcache.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 echo "echo 3 > /proc/sys/vm/drop_caches"
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m↑↓↑↓※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※↓↑↓↑"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m                  Cache  Cleared                  "
+echo -e "\e[92m                  Cache  Cleared                  "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m↑↓↑↓※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※↓↑↓↑\e[0m"
