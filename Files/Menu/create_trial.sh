657509854d715cb4d895ec12f902511bbd131faf
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:17:09 +0800

    Update create_trial.sh

--- Files/Menu/create_trial.sh
+++ Files/Menu/create_trial.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : beeggy
 MYIP=$(wget -qO- ipv4.icanhazip.com)
 User=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
@@ -7,7 +7,7 @@ Days="1"
 Today=`date +%s`
 Days_Detailed=$(( $Days * 86400 ))
 Expire_On=$(($Today + $Days_Detailed))
-Expiration=$(date -u --date="1970-01-01 $Expire_On sec GMT" +%Y/%m/%d)
+Expiration=$(date -u --date="2019-01-01 $Expire_On sec GMT" +%Y/%m/%d)
 Expiration_Display=$(date -u --date="1970-01-01 $Expire_On sec GMT" '+%d %b %Y')
 opensshport="$(netstat -ntlp | grep -i ssh | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 dropbearport="$(netstat -nlpt | grep -i dropbear | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -17,9 +17,9 @@ squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
 useradd -e `date -d "$Days days" +"%Y-%m-%d"` -s /bin/false -M $User
 echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
 clear
-echo -e "\e[94m[][][]=====================================[][][]"
+echo -e "\e[94m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[93m           AutoScriptVPS by beeggy           "
 echo -e "\e[0m                                                   "
 echo -e "         Username        :  $User"
 echo -e "         Password        :  $Pass"
@@ -33,4 +33,4 @@ echo -e "         Port Squid      :  "$squidport
 echo -e "         Port OpenVPN    :  "$openvpnport
 echo -e "              $MYIP/client.ovpn"
 echo -e "                                                  "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[94m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
