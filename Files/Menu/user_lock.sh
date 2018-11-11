00f283c7916740b60a61727ad5ee74d23b05b22d
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:01:01 +0800

    Update user_lock.sh

--- Files/Menu/user_lock.sh
+++ Files/Menu/user_lock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
 read -p "         Username       :  " User
 egrep "^$User" /etc/passwd >/dev/null
@@ -14,25 +14,25 @@ if grep -Fxq "$User" /etc/Locked_List.txt
 then
   clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
-echo -e "\e[91m           User Has Been Locked Already           "
+echo -e "\e[94m           User Has Been Locked Already           "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 exit
 else
   clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
-echo -e "\e[91m               User Has Been Locked               "
-echo -e "\e[91m        User Has Been Added To Locked_List        "
+echo -e "\e[94m               User Has Been Locked               "
+echo -e "\e[3m        User Has Been Added To Locked_List        "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 echo $User >> /etc/Locked_List.txt
 passwd -l $User
 fi
@@ -40,13 +40,13 @@ fi
 else
 	clear
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 	echo -e "\e[0m                                                   "
-	echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+	echo -e "\e[91m           AutoScriptVPS by  Beeggy           "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[91m              Username Doesnt Exist               "
+	echo -e "\e[92m              Username Doesnt Exist               "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]\e[0m"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 	exit
     exit 1
 fi
