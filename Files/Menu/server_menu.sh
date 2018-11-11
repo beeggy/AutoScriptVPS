7c96a958d964595e94bdd40cc58b65de40a920ad
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:29:48 +0800

    Update server_menu.sh

--- Files/Menu/server_menu.sh
+++ Files/Menu/server_menu.sh
@@ -1,67 +1,67 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  _Dreyannz_           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m            [1] Change Server Password"
-echo -e "\e[93m            [2] RAM Usage"
-echo -e "\e[93m            [3] Diagnostics"
-echo -e "\e[93m            [4] Benchmark"
-echo -e "\e[93m            [5] SpeedTest"
-echo -e "\e[93m            [x] Exit"
+echo -e "\e[32m            [1] Change Server Password"
+echo -e "\e[33m            [2] RAM Usage"
+echo -e "\e[34m            [3] Diagnostics"
+echo -e "\e[35m            [4] Benchmark"
+echo -e "\e[36m            [5] SpeedTest"
+echo -e "\e[37m            [x] Exit"
 echo -e "\e[0m                                                   "
 read -p "       Select From Options [1-7 or x] :  " Server
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 case $Server in
 		1)
 		clear
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+		echo -e "\e[92m           AutoScriptVPS by  Beeggy            "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m             Checking root Permissions            "
+		echo -e "\e[92m             Checking root Permissions            "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 		sleep 4
 		if [[ $USER != "root" ]]; then
 		clear
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+		echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[91m              root Permissions Denied             "
+		echo -e "\e[93m              root Permissions Denied             "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 		exit
 		fi
 		clear
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+		echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 		echo -e "\e[0m                                                   "
 		read -p "         New Password   :  " Pass
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 		sleep 4
 		New_Pass=root:$Pass;
 		echo $New_Pass > passwd
 		chpasswd < passwd
 		rm passwd
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 		echo -e "\e[0m                                                   "
-		echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+		echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m            Password Has Been Changed             "
+		echo -e "\e[93m            Password Has Been Changed             "
 		echo -e "\e[0m                                                   "
-		echo -e "\e[94m[][][]======================================[][][]\e[0m"
+		echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 		exit
 		;;
 		2)
