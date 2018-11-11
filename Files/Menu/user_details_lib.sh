d91cf1530ffe5512bc1730f5c83978bd18eb629a
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:33:13 +0800

    Update user_details_lib.sh

--- Files/Menu/user_details_lib.sh
+++ Files/Menu/user_details_lib.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 Username=`cat /etc/passwd | grep -Ew ^$1 | cut -d":" -f1`
 if [ "$Username" = "" ]
 then
 	clear
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 	echo -e "\e[0m                                                   "
-	echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+	echo -e "\e[92m           AutoScriptVPS by Beeggy           "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[93m              Username Doesnt Exist               "
+	echo -e "\e[92m              Username Doesnt Exist               "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]\e[0m"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 	exit
 fi
 Userid=`cat /etc/passwd | grep -Ew ^$Username | cut -d":" -f3`
@@ -27,12 +27,12 @@ AccountExpiryDate=`chage -l $Username | grep "Account expires" | awk -F": " '{pr
 HomeDirSize=`du -hs $UserHomeDir | awk '{print $1}'`
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
 printf "%-25s : %5s\n"   "      Username                 " "$Username"
 printf "%-25s : %5s\n"   "      Password previous change " "$LastPasswordChangeDate"
 printf "%-25s : %5s\n"   "      Account Date Expired     " "$AccountExpiryDate"
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
