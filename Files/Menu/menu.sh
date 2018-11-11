7acefa88f02754cd1471edad95f8510efca92ec1
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:27:12 +0800

    Update menu.sh

--- Files/Menu/menu.sh
+++ Files/Menu/menu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m    :::::::::  :::::::::   ::::::::  :::   :::    "
-echo -e "\e[94m    :+:    :+: :+:    :+: :+:    :+: :+:   :+:    "
-echo -e "\e[94m    +:+    +:+ +:+    +:+        +:+  +:+ +:+     "
-echo -e "\e[94m    +#+    +:+ +#++:++#:      +#++:    +#++:      "
-echo -e "\e[94m    +#+    +#+ +#+    +#+        +#+    +#+       "
-echo -e "\e[94m    #+#    #+# #+#    #+# #+#    #+#    #+#       "
-echo -e "\e[94m    #########  ###    ###  ########     ###       "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[32m              «»«»«»«»«»«»«»«»                    "
+echo -e "\e[32m            ˙°˙°˙°˙°˙°˙°˙°˙°˙°˙°                 "
+echo -e "\e[32m            ˙°˙°˙°˙°˙°˙°˙°˙°˙°˙°                  "
+echo -e "\e[31m           ←Modified by Beeggy→                  "
+echo -e "\e[32m            ●○●○●○●○●○●○●○●○●○●                  "
+echo -e "\e[31m             ^<TEAM EPIPHANY>^                    "
+echo -e "\e[32m           ★✩★✩★✩★✩★✩★✩★                   "
+echo -e "\e[93m          AutoScriptVPS by Beeggy                 "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m             [accounts/options/server]            "
+echo -e "\e[35m         [accounts/options/server]                "
 echo -e "\e[0m                                                   "
