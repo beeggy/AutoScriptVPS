3eefdb623b43e857cd2526000fab57f522951f5e
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:31:39 +0800

    Update show_ports.sh

--- Files/Menu/show_ports.sh
+++ Files/Menu/show_ports.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 opensshport="$(netstat -ntlp | grep -i ssh | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 dropbearport="$(netstat -nlpt | grep -i dropbear | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 stunnel4port="$(netstat -nlpt | grep -i stunnel | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -7,9 +7,9 @@ openvpnport="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $
 squidport="$(cat /etc/squid3/squid.conf | grep -i http_port | awk '{print $2}')"
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[93m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
 echo -e "         Port OpenSSH    :  "$opensshport
 echo -e "         Port Dropbear   :  "$dropbearport
@@ -17,4 +17,4 @@ echo -e "         Port SSL        :  "$stunnel4port
 echo -e "         Port Squid      :  "$squidport
 echo -e "         Port OpenVPN    :  "$openvpnport
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
