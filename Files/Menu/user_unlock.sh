bd96b0ba1343b8d715470af5926c89d2642871d6
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:00:33 +0800

    Update user_unlock.sh

--- Files/Menu/user_unlock.sh
+++ Files/Menu/user_unlock.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[94m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[91m           AutoScriptVPS by Beeggy           "
 echo -e "\e[0m                                                   "
 read -p "         Username       :  " User
 egrep "^$User" /etc/passwd >/dev/null
@@ -14,14 +14,14 @@ if grep -Fxq "$User" /etc/Locked_List.txt
 then
   clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[91m           AutoScriptVPS by  _Dreyannz_           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m              User Has Been Unlocked              "
-echo -e "\e[93m      User Has Been Removed from Locked_List      "
+echo -e "\e[92m              User Has Been Unlocked              "
+echo -e "\e[92m      User Has Been Removed from Locked_List      "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 passwd -u $User
 sed -i 's/$User//g' /etc/Locked_List.txt
 
@@ -29,26 +29,26 @@ exit
 else
   clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[91m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m                User Is Not Locked                "
+echo -e "\e[92m                User Is Not Locked                "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 exit
 fi
 
 else
 	clear
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 	echo -e "\e[0m                                                   "
-	echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+	echo -e "\e[91m           AutoScriptVPS by  Beeggy           "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[91m              Username Doesnt Exist               "
+	echo -e "\e[92m              Username Doesnt Exist               "
 	echo -e "\e[0m                                                   "
-	echo -e "\e[94m[][][]======================================[][][]\e[0m"
+	echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 	exit
     exit 1
 fi
