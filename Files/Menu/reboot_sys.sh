697f4cf463130c77dfb2dbf125f7b00d59233a37
beeggy <44807394+beeggy@users.noreply.github.com>
2018-11-11 18:28:10 +0800

    Update reboot_sys.sh

--- Files/Menu/reboot_sys.sh
+++ Files/Menu/reboot_sys.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# Script by : _Dreyannz_
+# Script by : Beeggy
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
+echo -e "\e[92m           AutoScriptVPS by  Beeggy           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m            [1] Reboot Now"
-echo -e "\e[93m            [2] Reboot Time"
-echo -e "\e[93m            [x] Exit"
+echo -e "\e[32m            [1] Reboot Now"
+echo -e "\e[33m            [2] Reboot Time"
+echo -e "\e[34m            [x] Exit"
 echo -e "\e[0m                                                   "
 read -p "       Select From Options [1-3 or x] :  " Reboot
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]======================================[][][]\e[0m"
+echo -e "\e[95m※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※\e[0m"
 sleep 3
 clear
 case $Reboot in
